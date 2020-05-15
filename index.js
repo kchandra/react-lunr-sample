b0f88473b19a6f0923e3a718756178acc80d558f
Kushal Chandra <kushalc@zillow.com>
2020-05-15 08:49:21 -0700

    added some logging

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var fs = require('fs');
 var path = require('path');
 
 exports.get = function(event, context, callback) {
+  JSON.stringify(event);
+  JSON.stringify(context);
+  JSON.stringify(callback);
+  
   var contents = fs.readFileSync(`build${path.sep}index.html`);
   var result = {
     statusCode: 200,
