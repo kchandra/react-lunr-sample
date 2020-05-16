c177df95bfeca9c826628b0697c18ca160daca3d
Kushal Chandra <kushalc@zillow.com>
2020-05-15 23:18:06 -0700

    proxy and root route

--- index.js
+++ index.js
@@ -17,18 +17,3 @@ exports.get = function(event, context, callback) {
 
   callback(null, result);
 };
-
-exports.proxy = function(event, context, callback) {
-  console.log(JSON.stringify(event));
-  console.log(JSON.stringify(context));
-  console.log(JSON.stringify(callback));
-  
-  var contents = fs.readFileSync(`build${path.sep}index.html`);
-  var result = {
-    statusCode: 200,
-    body: contents.toString(),
-    headers: {'content-type': 'text/html'}
-  };
-
-  callback(null, result);
-};
