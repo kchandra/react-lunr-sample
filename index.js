6b057b2f8efb34c43861f54eef0a4be73aa4eba8
Kushal Chandra <kushalc@zillow.com>
2020-05-15 22:34:02 -0700

    update lambda name

--- index.js
+++ index.js
@@ -4,9 +4,24 @@ var fs = require('fs');
 var path = require('path');
 
 exports.get = function(event, context, callback) {
-  JSON.stringify(event);
-  JSON.stringify(context);
-  JSON.stringify(callback);
+  console.log(JSON.stringify(event));
+  console.log(JSON.stringify(context));
+  console.log(JSON.stringify(callback));
+  
+  var contents = fs.readFileSync(`build${path.sep}index.html`);
+  var result = {
+    statusCode: 200,
+    body: contents.toString(),
+    headers: {'content-type': 'text/html'}
+  };
+
+  callback(null, result);
+};
+
+exports.proxy = function(event, context, callback) {
+  console.log(JSON.stringify(event));
+  console.log(JSON.stringify(context));
+  console.log(JSON.stringify(callback));
   
   var contents = fs.readFileSync(`build${path.sep}index.html`);
   var result = {
