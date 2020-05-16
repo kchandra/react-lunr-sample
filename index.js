92d678567801c793f2c545abbbe710e044431f90
Kushal Chandra <kushalc@zillow.com>
2020-05-16 10:11:52 -0700

    update code to serve based on proxy

--- index.js
+++ index.js
@@ -2,18 +2,34 @@
 
 var fs = require('fs');
 var path = require('path');
+var mime = require('mime-types')
 
 exports.get = function(event, context, callback) {
   console.log(JSON.stringify(event));
   console.log(JSON.stringify(context));
   console.log(JSON.stringify(callback));
-  
-  var contents = fs.readFileSync(`build${path.sep}index.html`);
   var result = {
-    statusCode: 200,
-    body: contents.toString(),
-    headers: {'content-type': 'text/html'}
-  };
+    statusCode: 404
+  }
+  
+  if(event.resource === "/") {
+    const contents = fs.readFileSync(`build${path.sep}index.html`);
+    result = {
+      statusCode: 200,
+      body: contents.toString(),
+      headers: {'content-type': 'text/html'}
+    };
+  }
+  
+  if(event.resource == "{/proxy+}") {
+    const contents = fs.readFileSync(`build${event.path}`);
+    result = {
+      statusCode: 200,
+      body: contents.toString(),
+      headers: {'content-type': mime.lookup(event.path)}
+    };
+  }
+
 
   callback(null, result);
 };
